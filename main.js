3287f1d6364972829e6eaaf4e8f86dce5dc580a5
kadirkagancoskun <kadirkagancoskun@gamil.com>
2025-03-25 04:15:24 +0100

    Add cire game mechanic, make a func plays 5 rounds

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 console.log("Hi");
 
 //Define variables plasyersChoice, computerChoice, playerScore, computerScore
+let computerChoice;
 let playerChoice;
-let computerChoice
-let playerScore;
-let computerScore;
+let playerScore=0;
+let computerScore=0;
 
 //Get computers choice
 
@@ -17,20 +17,52 @@ function getComputerChoice () {
         computerChoice="scissor";
     }
 
-    console.log("Computers choice for this round: " + computerChoice) ;
+    return computerChoice;
 }
-
 
 //Get users choice
 function getPlayerChoice (){
 playerChoice = prompt("Please choose one: rock, paper, scissor", "Harry Potter");
-console.log("Players choice for this round: " + playerChoice);
+return playerChoice;
 }
 
-getComputerChoice();
-getPlayerChoice();
+//Play a round and decide the winner of the round and increase/decrease corresponding score
 
-//Decide the winner of the round and keep track of points
+function playRound(a, b){
 
-//Function that plays five rounds
+    if(a=="rock"&b=="scissor"){
+        computerScore ++;
+        return `Winner is computer. Score: player: ${playerScore} computer: ${computerScore}`
+    } else if (a=="paper"&b=="rock"){
+        computerScore ++;
+        return `Winner is computer. Score: player: ${playerScore} computer: ${computerScore}`
+    } else if (a=="scissor"&b=="paper"){
+        computerScore ++;
+       return `Winner is computer. Score: player: ${playerScore} computer: ${computerScore}`
+    } else if (a===b){
+        return `No winner. Score: player: ${playerScore} computer: ${computerScore}`
+    } else {
+        playerScore ++;
+        return `Winner is player. Score: player: ${playerScore} computer: ${computerScore}`
+    }
+}
 
+
+//Function that plays the game best of five
+function playFiveGames(){
+    for (let i = 0; i<5; i++){
+        let a = getComputerChoice();
+        let b = getPlayerChoice();
+    console.log(playRound(a,b));
+    if(playerScore === 3 || computerScore === 3){
+        break;
+    }
+    }
+    if(playerScore>computerScore){
+    console.log(`Game is over. The winner is player! Score: player: ${playerScore} computer: ${computerScore}`)
+    }else{
+    console.log(`Game is over. Winner is computer. Score: player: ${playerScore} computer: ${computerScore}`)    
+    }
+}
+
+playFiveGames();
